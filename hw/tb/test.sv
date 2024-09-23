b8884026a462f4a801aa1a335d19b3d00db5ee54
cirofabianbermudez <cirofabian.bermudez@gmail.com>
2024-09-23 09:32:44 +0200

    feat: autodetect error

--- hw/tb/test.sv
+++ hw/tb/test.sv
@@ -1,5 +1,5 @@
 module test (
-    debouncer_if.dvr db_if
+    debouncer_if vif
 );
 
   int debounce_time;
@@ -8,42 +8,88 @@ module test (
     $display("Begin Of Simulation.");
     debounce_time = 120;
     reset();
-    normal();
-    bounce();
+    fork
+
+      begin
+        normal();
+        bounce();
+      end
+
+      begin
+        check_db_tick();
+      end
+
+    join
+
     $display("End Of Simulation.");
     $finish;
   end
 
   task automatic reset();
-    db_if.rst_i = 1'b1;
-    db_if.sw_i  = 1'b0;
-    repeat (2) @(db_if.cb);
-    db_if.cb.rst_i <= 1'b0;
-    repeat (debounce_time) @(db_if.cb);
+    vif.rst_i = 1'b1;
+    vif.sw_i  = 1'b0;
+    repeat (2) @(vif.cb);
+    vif.cb.rst_i <= 1'b0;
+    repeat (debounce_time) @(vif.cb);
   endtask : reset
 
   task automatic normal();
-    db_if.cb.sw_i <= 1'b1;
-    repeat (debounce_time) @(db_if.cb);
-    db_if.cb.sw_i <= 1'b0;
-    repeat (debounce_time) @(db_if.cb);
+    vif.cb.sw_i <= 1'b1;
+    repeat (debounce_time) @(vif.cb);
+    vif.cb.sw_i <= 1'b0;
+    repeat (debounce_time) @(vif.cb);
   endtask : normal
 
   task automatic bounce();
     int delay1, delay2;
     realtime time1, time2;
     for (int i = 0; i < 1000; i++) begin
-      db_if.cb.sw_i <= 1'b1;
+      delay1 = $urandom_range(debounce_time / 10, debounce_time);
+      delay2 = $urandom_range(debounce_time / 10, debounce_time);
+      $display("[INFO] %4t: iter = %3d, delay1 = %10d, delay2 = %10d", $realtime, i, delay1, delay2);
+      vif.cb.sw_i <= 1'b1;
       time1  = $realtime;
-      delay1 = $urandom_range(debounce_time / 2, debounce_time);
-      repeat (delay1) @(db_if.cb);
-      db_if.cb.sw_i <= 1'b0;
-      delay2 = $urandom_range(debounce_time / 2, debounce_time);
+      repeat (delay1) @(vif.cb);
+      vif.cb.sw_i <= 1'b0;
       time2  = $realtime;
-      repeat (delay2) @(db_if.cb);
-      $display("iter = %3d, delay1 = %3d, delay2 = %3d, time1 = %t, time2 = %t", i, delay1, delay2,
-               time1, time2);
+      repeat (delay2) @(vif.cb);
+      $display("[INFO] %4t: iter = %3d, time1  = %t, time2  = %t", $realtime, i, time1, time2);
     end
   endtask : bounce
+
+  task automatic check_db_tick();
+    int tick_counter = 0;
+    int tick_error_counter = 0;
+    forever begin
+
+      fork
+        begin : wd_timer_fork
+          fork : db_tick_wd_timer
+            begin 
+              wait (vif.cb.db_tick_o != 1);
+              @(vif.cb iff (vif.cb.db_tick_o == 1));
+              tick_counter++;
+              $display("[INFO] %4t: posedge db_tick_o, num_pulses: %4d", $realtime, tick_counter);
+            end
+            begin
+              repeat (10_000) @(vif.cb);
+              $display("[INFO] %4t: Timed out!", $realtime);
+              $display("[INFO] %4t: tick_counter: %4d, tick_error_counter: %4d", $realtime, tick_counter, tick_error_counter);
+              $finish;
+            end
+          join_any : db_tick_wd_timer
+          disable fork;
+        end : wd_timer_fork
+      join
+
+      @(vif.cb)
+      if (vif.cb.db_tick_o == 1) begin
+        tick_error_counter++;
+        $display("[ERROR] %4t: db_tick_o ", $realtime);
+        //$finish;
+      end
+
+    end
+  endtask : check_db_tick
 
 endmodule : test
